34ef7afa10a9f8b6d3f3322ce8b0eb515cb632df
CrXane <25283652+CrXane@users.noreply.github.com>
2020-07-03 02:15:49 -0400

    Update memes.py

--- memes.py
+++ memes.py
@@ -1,51 +1,54 @@
 # pip install fake-useragent
 # pip install discord-webhook
 
-import sys
-import time
-import random
-import requests
-from fake_useragent import UserAgent
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import time # Timer
+import random # Color randomizer
+import requests # GET method
+from fake_useragent import UserAgent # Get memes
+from discord_webhook import DiscordWebhook, DiscordEmbed # Send messages
 
-starttime = time.time()
-refreshtime = 30 # seconds
+starttime = time.time() # Start time
+refreshtime = 30 # Seconds
 
-forward = "webhook-url"
+# Webhook url
+forward = "https://discordapp.com/api/webhooks/728382218636099604/4t9ScuBxxpFexevkLY8pzYE7En12I2v9BMv6evKib-Pfg9D_5ksvC5ha6a3ZUkzdHT49"
 
+# Subreddits
 memelist = [
 	"memes",
 	"dankmemes",
 	"PewdiepieSubmissions"
 ]
 
+# Get meme
 def memes():
-	ua = UserAgent()
-	url = "https://www.reddit.com/r/" + random.choice(memelist) + "/randomrising/.json?kind=t3"
+	ua = UserAgent() # Fake agent
+	url = "https://www.reddit.com/r/" + random.choice(memelist) + "/randomrising/.json?kind=t3" # Json url
+	response = requests.get(url, headers={'User-agent': ua.random}) # GET method
 
-	response = requests.get(url, headers={'User-agent': ua.random})
-
-	if response.ok:
-		output = response.json()['data']['children']
+	if response.ok: # Connected to server
+		output = response.json()['data']['children'] # Get json output
 		
-		for child in range(25):
-			data = output[child]['data']
+		for child in range(25): # Loop through each entry
+			data = output[child]['data'] # Get entry
 			
-			if data:
-				if not data['over_18'] and not data['media']:
-					send_discord_message(data)
-					break
-		
+			if data: # Check if valid data
+				if not data['over_18'] and not data['media']: # SFW and non-media content (images)
+					send_discord_message(data) # Send discord message
+					break # Send only one message
+
+# Send discord message	
 def send_discord_message(data):
-	temp = "by " + data['author'] + " on " + data['subreddit_name_prefixed'] + "\n" + str(data['ups']) + " likes - " + str(data['num_comments']) + " comments"
-	webhook = DiscordWebhook(url=forward)
-	embed = DiscordEmbed(color=random.randint(0, 16777215))
-	embed.set_author(name=data['title'], url='https://reddit.com' + data['permalink'], icon_url=data['thumbnail'])
-	embed.set_footer(text=temp)
-	embed.set_image(url=data['url'])
-	webhook.add_embed(embed)
-	response = webhook.execute()
+	temp = "by " + data['author'] + " on " + data['subreddit_name_prefixed'] + "\n" + str(data['ups']) + " likes - " + str(data['num_comments']) + " comments" # Footer
+	webhook = DiscordWebhook(url=forward) # Webhook url
+	embed = DiscordEmbed(color=random.randint(0, 16777215)) # Side color
+	embed.set_author(name=data['title'], url='https://reddit.com' + data['permalink'], icon_url=data['thumbnail']) # Title
+	embed.set_footer(text=temp) # Footer text
+	embed.set_image(url=data['url']) # Image
+	webhook.add_embed(embed) # Merge embeds data
+	response = webhook.execute() # POST method and receive output
 	
-while True:
-	memes()
-	time.sleep(float(refreshtime) - ((time.time() - starttime) % float(refreshtime)))
+# Main func
+#while True:
+memes() # MemeBot
+#	time.sleep(float(refreshtime) - ((time.time() - starttime) % float(refreshtime))) # Timer
